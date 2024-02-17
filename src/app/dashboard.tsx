a8bd1c53744d350427a15f5ce2199d9b6aa500cd
Nazar Vovk <nazarvovk.work@gmail.com>
2024-02-17 11:52:56 +0200

    feat: basic package info

--- src/app/dashboard.tsx
+++ src/app/dashboard.tsx
@@ -1,5 +1,6 @@
 import { Dependencies, DependenciesSchema, PackageJSON } from '@/lib/utils'
 import { DependencyTable } from './dependency-table'
+import { ArchiveIcon } from '@radix-ui/react-icons'
 
 type DashboardProps = {
   packageJson: PackageJSON
@@ -26,10 +27,35 @@ export const Dashboard = (props: DashboardProps) => {
   const dependencies = getDependencies(packageJson)
 
   return (
-    <div className='grid gap-4 md:grid-cols-2'>
-      {Object.entries(dependencies).map(([key, deps]) => {
-        return <DependencyTable key={key} label={key} dependencies={deps} />
-      })}
+    <div>
+      <div className='mb-2 flex items-center justify-between gap-2'>
+        <h2 className='flex items-center gap-2 text-2xl font-bold'>
+          <ArchiveIcon className='inline-block size-8' />
+          {packageJson.name ? (
+            <span className='text-blue-500'>{packageJson.name}</span>
+          ) : (
+            <span className='rounded-md border border-dashed bg-rose-100 px-2 text-rose-700'>
+              No &quot;name&quot; field in the file
+            </span>
+          )}
+          {packageJson.version && (
+            <span className='rounded-md border bg-zinc-100 px-2 py-1 text-xl text-zinc-700'>
+              {packageJson.version}
+            </span>
+          )}
+        </h2>
+      </div>
+      {packageJson.description && (
+        <p className='mb-2 text-sm text-zinc-500'>{packageJson.description}</p>
+      )}
+      <hr className='mb-2' />
+
+      <h3 className='mb-2 text-xl font-bold'>Dependencies:</h3>
+      <div className='grid gap-4 md:grid-cols-2'>
+        {Object.entries(dependencies).map(([key, deps]) => {
+          return <DependencyTable key={key} label={`"${key}"`} dependencies={deps} />
+        })}
+      </div>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center gap-8 px-8 py-24'>
+    <main className='flex min-h-screen flex-col items-center gap-2 p-8'>
       <h1 className='text-2xl font-bold'>RePackage.JSON</h1>
-      <p>
+      <p className='text-xs text-zinc-500'>
         This app is fully client-side and does not send or store any data. Your file is processed in
         your browser.
       </p>
