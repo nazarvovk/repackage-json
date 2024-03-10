70c89950aa0916d5e11b188c13cef28ee5a27fa3
Nazar Vovk <nazarvovk.work@gmail.com>
2024-03-10 11:28:34 +0200

    chore: lint

--- src/components/table/utils.tsx
+++ src/components/table/utils.tsx
@@ -1,32 +1,39 @@
-import { ChevronDownIcon } from '@radix-ui/react-icons';
-import { HeaderContext, SortingFn } from '@tanstack/react-table';
-import { Button } from '../ui/button';
-import { cn } from '@/lib/utils';
+import { ChevronDownIcon } from '@radix-ui/react-icons'
+import { HeaderContext, SortingFn } from '@tanstack/react-table'
+import { Button } from '../ui/button'
+import { cn } from '@/lib/utils'
 
-export function generateTableHeader<T = any>(title: string) {
-  return ({ column }: HeaderContext<T, unknown>) => {
-    const sorted = column.getIsSorted();
-    const filtered = column.getIsFiltered();
+export function generateTableHeader<T = unknown>(title: string) {
+  const Header = ({ column }: HeaderContext<T, unknown>) => {
+    const sorted = column.getIsSorted()
+    const filtered = column.getIsFiltered()
 
     return (
-      <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+      <Button variant='ghost' onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
         {title}
-        {sorted && <ChevronDownIcon className={cn('ml-2 h-4 w-4 transition', { 'rotate-180': sorted === 'desc' })} />}
-        {filtered && <span className="ml-2">Filtered</span>}
+        {sorted && (
+          <ChevronDownIcon
+            className={cn('ml-2 size-4 transition', { 'rotate-180': sorted === 'desc' })}
+          />
+        )}
+        {filtered && <span className='ml-2'>Filtered</span>}
       </Button>
-    );
-  };
+    )
+  }
+  Header.displayName = `Header-${title}`
+
+  return Header
 }
 
-export const tableSortingFns: { [key: string]: SortingFn<any> } = {
+export const tableSortingFns: { [key: string]: SortingFn<unknown> } = {
   length: (rowA, rowB, id) => {
-    const a: any[] = rowA.getValue(id);
-    const b: any[] = rowB.getValue(id);
-    return b.length - a.length;
+    const a: unknown[] = rowA.getValue(id)
+    const b: unknown[] = rowB.getValue(id)
+    return b.length - a.length
   },
   bool: (rowA, rowB, id) => {
-    const a = rowA.getValue(id);
-    const b = rowB.getValue(id);
-    return a === b ? 0 : a ? -1 : 1;
+    const a = rowA.getValue(id)
+    const b = rowB.getValue(id)
+    return a === b ? 0 : a ? -1 : 1
   },
-};
+}
